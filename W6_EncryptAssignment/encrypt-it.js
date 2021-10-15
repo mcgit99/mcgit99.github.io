13aaa2c1864c727d558ce251d35b72fa9c2ef9b7
mcgit99 <mark-josephcortez@csus.edu>
2021-10-15 11:50:33 -0700

    W6 Assignment
    Adding W6 files to Repo. Still a WIP.

---
+++ W6_EncryptAssignment/encrypt-it.js
@@ -0,0 +1,56 @@
+/*
+ * Starter file 
+ */
+(function() {
+  "use strict";
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window, signalling the HTML DOM has been constructed
+   * on the page. When this event occurs, the attached function (init) will be called.
+   */
+  window.addEventListener("load", init);
+  /**
+   * TODO: Write a function comment using JSDoc.
+   */
+  function init() {
+    // Note: In this function, we usually want to set up our event handlers
+    // for UI elements on the page.
+    encryptOnClick();
+  }
+
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+  function encryptOnClick() {
+    var encryptButton = document.getElementById('encrypt-it');
+    var userInput = document.getElementById('input-text');
+    var shifted;
+   
+    encryptButton.addEventListener('click', () => {
+      shifted = shiftCipher(userInput);
+      document.getElementById('result').innerHTML = shifted;
+      console.log("Button Clicked!");   
+    })
+  }
+
+  function shiftCipher(text) {
+    text = text.toString().toLowerCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      } else if (text[i] == 'z') {
+        result += 'a';
+      } else { // letter is between 'a' and 'y'
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
+    }
+    return result;
+  }
+
+})();
+
+
